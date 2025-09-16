ea03ea1d3f2620af58bc9ada07e958273f85ae18
Gustavo Almansa <gustavosalmansa@gmail.com>
2025-09-16 16:54:45 -0300

    subindo projetos

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using EnumStudy.Entities;
+using EnumStudy.Entities.Enums;
+namespace EnumStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = Entities.Enums.OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
