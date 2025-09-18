0bacfa2c290bb832c58e711f925e353e6594db1e
Gustavo Almansa <gustavosalmansa@gmail.com>
2025-09-17 22:56:32 -0300

    Account project

---
+++ Account/Account/Entities/Account.cs
@@ -0,0 +1,26 @@
+﻿namespace Account.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public decimal Balance { get; protected set; }
+
+        public Account() { }
+        public Account(int number, string holder, decimal balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ Account/Account/Entities/BusinessAccount.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Account.Entities
+{
+     class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        public BusinessAccount() { }
+        public BusinessAccount(int number, string holder, decimal balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += (decimal)amount;
+            }
+        }
+    }
+}
