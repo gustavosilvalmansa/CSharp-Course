cd1bf283178ae72568b14475525092ebf9f537a8
Gustavo Almansa <gustavosalmansa@gmail.com>
2025-09-19 11:17:01 -0300

    Creating ReservationClass

---
+++ Reservation/Reservation/Entities/Reservation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Reservation.Entities
+{
+    class ReservationClass
+    {
+        public int roomNumber { get; set; }
+
+        public DateTime checkIn { get; set; }
+        public DateTime checkOut { get; set; }
+        public ReservationClass()
+        {
+        }
+
+        public ReservationClass(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            this.roomNumber = roomNumber;
+            this.checkIn = checkIn;
+            this.checkOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = checkOut.Subtract(checkIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + roomNumber
+                + ", check-in: "
+                + checkIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + checkOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+
+
+    }
+}
---
+++ Reservation/Reservation/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Reservation.Entities;
+namespace Reservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                return;
+            }
+            else
+            {
+                ReservationClass reservation = new ReservationClass(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+        }
+    }
+}
