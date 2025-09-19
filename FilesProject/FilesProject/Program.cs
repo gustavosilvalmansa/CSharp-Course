fea05f25a92bd54c1ffd608923c18af0f6965612
Gustavo Almansa <gustavosalmansa@gmail.com>
2025-09-19 14:53:15 -0300

    Directory and DirectoryInfo

--- FilesProject/FilesProject/Program.cs
+++ FilesProject/FilesProject/Program.cs
@@ -7,31 +7,23 @@ namespace FilesProject
     {
         static void Main(string[] args)
         {
-            string path = @"C:\projetos\FilesProject\file.txt";
-            StreamReader sr = null;
+            string SourcePath = @"C:\projetos\FilesProject\";
             try
             {
-                sr = File.OpenText(path);
-                string content = sr.ReadToEnd();
-                Console.WriteLine(content);
-            }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine($"File not found: {ex.Message}");
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine($"Access denied: {ex.Message}");
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine($"I/O error: {ex.Message}");
-            }
-            finally
-            {
-                sr?.Close();
-            }
+                IEnumerable<string> folders = Directory.EnumerateDirectories(SourcePath, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Directory.CreateDirectory(SourcePath + @"newfolder");
 
+
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
