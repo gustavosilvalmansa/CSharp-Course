c8c5d4902f3cf97fbf9dadb84ed5fe2173ee0f23
Gustavo Almansa <gustavosalmansa@gmail.com>
2025-09-19 16:31:38 -0300

    Creating services class

---
+++ InterfacesProject/InterfacesProject/Services/RentalService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesProject.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+        private BrazilTaxService taxService = new BrazilTaxService();
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+        public void ProcessInvoice(Entities.CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            double tax = taxService.Tax(basicPayment);
+            carRental.Invoice = new Entities.Invoice(basicPayment, tax);
+
+        }
+    }
+}
